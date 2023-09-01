5bdb5ec5e62dfe495cc356a28fd175784c449837
Noah Attwood <github@noahattwood.com>
2023-08-31 22:29:28 -0300

    Adds main window that appears upon running the executable, improves CMake configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <QApplication>
+#include <QMainWindow>
 
-int main() {
-    std::cout << "Qt Test App!" << std::endl;
-    return 0;
+int main(int argc, char** argv ) {
+    QApplication qapp(argc, argv);
+
+    QMainWindow main_window;
+    main_window.setWindowTitle(QString("Qt Test App"));
+    main_window.setMinimumSize(200, 200);
+    main_window.show();
+
+    return qapp.exec();
 }
